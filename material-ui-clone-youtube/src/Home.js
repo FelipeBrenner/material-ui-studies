235fff5b9ed69add9513288417aace5ddcbb5dac
Felipe Brenner <felipeobrenner@hotmail.com>
2021-04-24 21:54:03 -0300

    Added videos

--- material-ui-clone-youtube/src/Home.js
+++ material-ui-clone-youtube/src/Home.js
@@ -14,6 +14,8 @@ import {
   Box,
   Typography,
   ListSubheader,
+  Grid,
+  Hidden,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
@@ -123,6 +125,88 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const videos = [
+  {
+    id: 1,
+    title:
+      "FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29",
+    channel: "Lucas Nhimi",
+    views: "11 mi de visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb1.png",
+  },
+  {
+    id: 2,
+    title:
+      "COMO MELHORAR SEU CODIGO JAVASCRIPT (ESLINT + PRETTIER + EDITORCONFIG) | Dicas e Truques #02",
+    channel: "Lucas Nhimi",
+    views: "957 mil visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb2.png",
+  },
+  {
+    id: 3,
+    title:
+      "CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27",
+    channel: "Lucas Nhimi",
+    views: "106 mil visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb3.png",
+  },
+  {
+    id: 4,
+    title:
+      "CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27",
+    channel: "Lucas Nhimi",
+    views: "5,6 mi de visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb4.png",
+  },
+  {
+    id: 5,
+    title:
+      "EDITOR DE POST COM MARKDOWN 2 | Criando uma Rede Social com React.js e .NET Core #26",
+    channel: "Lucas Nhimi",
+    views: "2,2 mi de visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb5.png",
+  },
+  {
+    id: 6,
+    title: "COMO MIGRAR PARA REACT HOOKS | Dicas e Truques #01",
+    channel: "Lucas Nhimi",
+    views: "233 mil visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb6.png",
+  },
+  {
+    id: 7,
+    title:
+      "PRÉ-REQUISITOS | Criando uma Rede Social com React.js e .NET Core #01",
+    channel: "Lucas Nhimi",
+    views: "118 mil visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb7.png",
+  },
+  {
+    id: 8,
+    title:
+      "GIT E GITHUB | Criando uma Rede Social com React.js e .NET Core #04",
+    channel: "Lucas Nhimi",
+    views: "1,9 mi de visualizações",
+    date: "há 1 semana",
+    avatar: "/images/avatar.jpeg",
+    thumb: "/images/thumb8.png",
+  },
+];
+
 function Home() {
   const classes = useStyles();
 
@@ -159,176 +243,223 @@ function Home() {
           </Button>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            {listItens[0].itens.map((item) => {
-              return (
-                <ListItem
-                  button
-                  key={item.name}
-                  classes={{ root: classes.listItem }}
-                >
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText
-                    classes={{
-                      primary: classes.listItemText,
-                    }}
-                    primary={item.name}
-                  />
-                </ListItem>
-              );
-            })}
-          </List>
-          <Divider />
-          <List>
-            {listItens[1].itens.map((item) => {
-              return (
-                <ListItem
-                  button
-                  key={item.name}
-                  classes={{ root: classes.listItem }}
-                >
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText
-                    classes={{
-                      primary: classes.listItemText,
-                    }}
-                    primary={item.name}
-                  />
-                </ListItem>
-              );
-            })}
-          </List>
-          <Divider />
-          <Box ml={6} mr={8} mt={4} mb={4}>
-            <Typography variant="body2">
-              Faça login para curtir videos, comentar e se inscrever.
-            </Typography>
-            <Box mt={2}>
-              <Button
-                startIcon={<AccountCircle />}
-                variant="outlined"
-                color="secondary"
-              >
-                Fazer Login
-              </Button>
-            </Box>
-          </Box>
-          <Divider />
-          <List
-            component="nav"
-            aria-labelledby="nested-list-subheader"
-            subheader={
-              <ListSubheader
-                component="div"
-                id="nested-list-subheader"
-                className={classes.subheader}
-              >
-                O Melhor do youtube
-              </ListSubheader>
-            }
+      <Box display="flex">
+        <Hidden mdDown>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
           >
-            {listItens[2].itens.map((item) => {
-              return (
-                <ListItem
-                  button
-                  key={item.name}
-                  classes={{ root: classes.listItem }}
-                >
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText
-                    classes={{
-                      primary: classes.listItemText,
-                    }}
-                    primary={item.name}
-                  />
-                </ListItem>
-              );
-            })}
-          </List>
-          <Divider />
-          <List>
-            {listItens[3].itens.map((item) => {
-              return (
-                <ListItem
-                  button
-                  key={item.name}
-                  classes={{ root: classes.listItem }}
-                >
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText
-                    classes={{
-                      primary: classes.listItemText,
-                    }}
-                    primary={item.name}
-                  />
-                </ListItem>
-              );
-            })}
-          </List>
-          <Divider />
-          <List
-            component="nav"
-            aria-labelledby="nested-list-subheader"
-            subheader={
-              <ListSubheader
-                component="div"
-                id="nested-list-subheader"
-                className={classes.subheader}
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+              <List>
+                {listItens[0].itens.map((item) => {
+                  return (
+                    <ListItem
+                      button
+                      key={item.name}
+                      classes={{ root: classes.listItem }}
+                    >
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText
+                        classes={{
+                          primary: classes.listItemText,
+                        }}
+                        primary={item.name}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Divider />
+              <List>
+                {listItens[1].itens.map((item) => {
+                  return (
+                    <ListItem
+                      button
+                      key={item.name}
+                      classes={{ root: classes.listItem }}
+                    >
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText
+                        classes={{
+                          primary: classes.listItemText,
+                        }}
+                        primary={item.name}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Divider />
+              <Box ml={6} mr={8} mt={4} mb={4}>
+                <Typography variant="body2">
+                  Faça login para curtir videos, comentar e se inscrever.
+                </Typography>
+                <Box mt={2}>
+                  <Button
+                    startIcon={<AccountCircle />}
+                    variant="outlined"
+                    color="secondary"
+                  >
+                    Fazer Login
+                  </Button>
+                </Box>
+              </Box>
+              <Divider />
+              <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                subheader={
+                  <ListSubheader
+                    component="div"
+                    id="nested-list-subheader"
+                    className={classes.subheader}
+                  >
+                    O Melhor do youtube
+                  </ListSubheader>
+                }
               >
-                Mais do YouTube
-              </ListSubheader>
-            }
+                {listItens[2].itens.map((item) => {
+                  return (
+                    <ListItem
+                      button
+                      key={item.name}
+                      classes={{ root: classes.listItem }}
+                    >
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText
+                        classes={{
+                          primary: classes.listItemText,
+                        }}
+                        primary={item.name}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Divider />
+              <List>
+                {listItens[3].itens.map((item) => {
+                  return (
+                    <ListItem
+                      button
+                      key={item.name}
+                      classes={{ root: classes.listItem }}
+                    >
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText
+                        classes={{
+                          primary: classes.listItemText,
+                        }}
+                        primary={item.name}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Divider />
+              <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                subheader={
+                  <ListSubheader
+                    component="div"
+                    id="nested-list-subheader"
+                    className={classes.subheader}
+                  >
+                    Mais do YouTube
+                  </ListSubheader>
+                }
+              >
+                {listItens[4].itens.map((item) => {
+                  return (
+                    <ListItem
+                      button
+                      key={item.name}
+                      classes={{ root: classes.listItem }}
+                    >
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText
+                        classes={{
+                          primary: classes.listItemText,
+                        }}
+                        primary={item.name}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Divider />
+              <List>
+                {listItens[5].itens.map((item) => {
+                  return (
+                    <ListItem
+                      button
+                      key={item.name}
+                      classes={{ root: classes.listItem }}
+                    >
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText
+                        classes={{
+                          primary: classes.listItemText,
+                        }}
+                        primary={item.name}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Divider />
+            </div>
+          </Drawer>
+        </Hidden>
+        <Box>
+          <Toolbar />
+          <Typography
+            variant="h5"
+            color="textPrimary"
+            style={{ fontWeight: 600 }}
           >
-            {listItens[4].itens.map((item) => {
-              return (
-                <ListItem
-                  button
-                  key={item.name}
-                  classes={{ root: classes.listItem }}
-                >
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText
-                    classes={{
-                      primary: classes.listItemText,
-                    }}
-                    primary={item.name}
+            Recomendados
+          </Typography>
+          <Grid container spacing={4}>
+            {videos.map((item, index) => (
+              <Grid item lg={3} md={4} sm={6} xs={12}>
+                <Box>
+                  <img
+                    style={{ width: "100%" }}
+                    alt={item.title}
+                    src={item.thumb}
                   />
-                </ListItem>
-              );
-            })}
-          </List>
-          <Divider />
-          <List>
-            {listItens[5].itens.map((item) => {
-              return (
-                <ListItem
-                  button
-                  key={item.name}
-                  classes={{ root: classes.listItem }}
-                >
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText
-                    classes={{
-                      primary: classes.listItemText,
-                    }}
-                    primary={item.name}
-                  />
-                </ListItem>
-              );
-            })}
-          </List>
-          <Divider />
-        </div>
-      </Drawer>
+                  <Box>
+                    <Typography
+                      style={{ fontWeight: 600 }}
+                      gutterBottom
+                      variant="body1"
+                      color="textPrimary"
+                    >
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      display="block"
+                      variant="body2"
+                      color="textSecondary"
+                    >
+                      {item.channel}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {`${item.views} • ${item.date}`}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Box>
     </div>
   );
 }
