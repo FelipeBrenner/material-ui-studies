480546cdf7105988ab16e2eb19201a3788892666
Felipe Brenner <felipeobrenner@hotmail.com>
2021-04-27 16:29:18 -0300

    Adding YouTube Data API

--- material-ui-clone-youtube/src/Home.js
+++ material-ui-clone-youtube/src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import clsx from "clsx";
 import {
   makeStyles,
@@ -31,6 +31,8 @@ import Subscriptions from "@material-ui/icons/Subscriptions";
 import VideoLibrary from "@material-ui/icons/VideoLibrary";
 import History from "@material-ui/icons/History";
 import AddCircle from "@material-ui/icons/AddCircle";
+
+import { api } from "./services/api";
 
 const drawerWidth = 240;
 
@@ -152,7 +154,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const videos = [
+/* const videos = [
   {
     id: 1,
     title: "LUCAS INUTILISMO - Flow Podcast #212",
@@ -241,12 +243,68 @@ const videos = [
     thumb:
       "https://i.ytimg.com/vi/oUuA_PGr1-Q/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAtqvzDz60oM9xhB--pjLZRm2T57Q",
   },
-];
+]; */
 
 function Home({ darkMode, setDarkMode }) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = useState(true);
+
+  const videos = [];
+
+  function viewsDescription(views) {
+    if (views < 1000000)
+      return `${views
+        .toString()
+        .substring(0, views.length - 3)} mil visualizações`;
+    else {
+      const decimal = Number(
+        views.toString().substring(views.length - 6, views.length - 5)
+      );
+
+      if (decimal)
+        return `${Number(
+          views.toString().substring(0, views.length - 6)
+        )},${decimal} mi de visualizações`;
+      else
+        return `${Number(
+          views.toString().substring(0, views.length - 6)
+        )} mi de visualizações`;
+    }
+  }
+
+  function dateDescription(publishedAt) {
+    const date = new Date(publishedAt);
+    const now = new Date();
+    const days =
+      Math.abs(now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
+    if (days < 7) {
+      return `há ${days.toFixed(0) - 1} dias`;
+    } else if (days < 30) {
+      return `há ${(days / 7).toFixed(0) - 1} semanas`;
+    } else if (days < 365) {
+      return `há ${(days / 30).toFixed(0) - 1} meses`;
+    } else return `há ${(days / 365).toFixed(0) - 1} anos`;
+  }
+
+  useEffect(() => {
+    api.get().then((response) => {
+      response.data.items.forEach((item) => {
+        videos.push({
+          title: item.snippet.title,
+          channel: item.snippet.channelTitle,
+          views: viewsDescription(item.statistics.viewCount),
+          date: dateDescription(item.snippet.publishedAt),
+          avatar: "https://avatars.githubusercontent.com/u/23401362?v=4",
+          thumb: item.snippet.thumbnails.maxres.url,
+        });
+      });
+    });
+  }, []);
+
+  console.log(videos);
+
+  // api.get().then((response) => console.log(response));
 
   const handleDrawer = () => {
     setOpen(!open);
---
+++ material-ui-clone-youtube/src/services/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const idVideos =
+  "qOer7KEMHIo" + // LUCAS INUTILISMO - Flow Podcast #212
+  "%2C" +
+  "yBwHwlM6PeA" + // ZOIO CANTA EMINEM E EMOCIONA A TODOS
+  "%2C" +
+  "mtcudot_Urs" + // DIOGO DEFANTE - Flow Podcast #184
+  "%2C" +
+  "EX4AyW4FI_4" + // POR ISSO QUE O WHINDERSSON NÃO FAZ COLLABS - SINUCASTIGO DA NOSTALGIA!!!
+  "%2C" +
+  "PImqHWNhLHc" + // Melhores momentos do Prior parte 1
+  "%2C" +
+  "oUuA_PGr1-Q" + // Dribles Mais Humilhantes de Neymar Jr
+  "%2C" +
+  "ve1_9okkuqA" + // TUDO PARECIA MAS NÃO ERA UM SONHO (FUNK REMIX) DJ LUCAS BEAT & ELIAS MONKBEL & CAIO COSTTA
+  "%2C" +
+  "mJnNCMrfCss"; // LUCAS - DJ LUCAS BEAT & CAMILLA E LUDMILLA
+
+export const api = axios.create({
+  baseURL:
+    "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=" +
+    idVideos +
+    "&key=AIzaSyDi4JkjcD1B42eaof_0S5Bh8s6um4U9xQI",
+});
